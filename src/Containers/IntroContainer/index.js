824487b3fa6901aeaed782fc70496a7e3d6a27f7
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-03-15 16:02:51 +0500

    adds intro-screen

--- src/Containers/IntroContainer/index.js
+++ src/Containers/IntroContainer/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Button, Input } from '../../Components';
-
-import { MainIntroContainer } from './style';
+import {
+  MainIntroContainer,
+  MainContainer,
+  ImageBackgroundMain,
+  MainBg,
+  ContentMainScreen,
+  HeadingH5,
+  HeadingSpan,
+  HeadingH1,
+  ContentP,
+  ContentFooterarea,
+  MtB5,
+  SigninBtn,
+  ContentDivButton,
+  SignUpWithEmail,
+} from './style';
 
 const IntroContainer = () => {
   return (
-    <MainIntroContainer>
-      <Button title={'signIn'} />
-      <Input placeholder="Signin" />
-    </MainIntroContainer>
+    <MainContainer>
+      <ImageBackgroundMain>
+        <MainBg>
+          <ContentMainScreen>
+            <HeadingH5>Welcome to</HeadingH5>
+            <HeadingH1>
+              CHEX
+            <HeadingSpan>.AI</HeadingSpan>
+            </HeadingH1>
+            <ContentP>Virtual Inspections</ContentP>
+            <ContentFooterarea>
+              <MtB5>
+                <SigninBtn to="/login">Sign in</SigninBtn>
+              </MtB5>
+              <ContentDivButton>
+                <SignUpWithEmail to="/register">Register</SignUpWithEmail>
+              </ContentDivButton>
+            </ContentFooterarea>
+          </ContentMainScreen>
+        </MainBg>
+      </ImageBackgroundMain>
+    </MainContainer>
   );
 };
 export default IntroContainer;
