20336f16fa72d3228e6e59d0c8b92ec5f0ee93f1
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-03-27 16:23:23 +0500

    merge branch

--- src/Components/InspectionCard/index.js
+++ src/Components/InspectionCard/index.js
@@ -4,16 +4,7 @@ import { AiFillCamera, AiOutlineDownload } from 'react-icons/ai';
 import { MdPlayArrow } from 'react-icons/md';
 import { RiDeleteBin5Line } from 'react-icons/ri';
 
-const InspectionCard = ({
-  title,
-  titletwo,
-  handleModal,
-  type,
-  category,
-  item,
-  groupType,
-  deleteFile,
-}) => {
+const InspectionCard = ({ title, titletwo, handleModal, type, category, item, groupType, deleteFile }) => {
   return (
     <div style={{ cursor: 'pointer' }}>
       <div onClick={() => handleModal(item, groupType)} style={{ background: 'none' }}>
@@ -21,24 +12,16 @@ const InspectionCard = ({
           item.type === 'Photo' ? (
             <img
               alt=""
+              className="inspection-image"
               // style={{ height: '140px', width: '80%' }}
               src={`${process.env.REACT_APP_AWS_S3_LINK}/${item.url}`}
             />
           ) : (
-            <video
-              style={{ height: '107px', width: '141px' }}
-              src={`${process.env.REACT_APP_AWS_S3_LINK}/${item.url}`}
-            />
+            <video className="inspection-image" src={`${process.env.REACT_APP_AWS_S3_LINK}/${item.url}`} />
           )
         ) : (
           <div className="vec-inspection-inspection_card">
-            <div className="vec-inspection-camera_icon">
-              {type === 'Photo' ? (
-                <AiFillCamera size={22} color="#97A9C5" />
-              ) : (
-                <MdPlayArrow size={22} color="#97A9C5" />
-              )}
-            </div>
+            <div className="vec-inspection-camera_icon">{type === 'Photo' ? <AiFillCamera size={22} color="#97A9C5" /> : <MdPlayArrow size={22} color="#97A9C5" />}</div>
             <div className="veh-inspection-downloadicon_center">
               <div className="veh-inspection-download_icon">
                 <AiOutlineDownload size={26} color="#1262B1" />
@@ -49,10 +32,7 @@ const InspectionCard = ({
       </div>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <p className="veh-inspection-registrationcard_text">{title}</p>
-        <div
-          style={{ marginLeft: '5px', marginTop: '10px' }}
-          onClick={() => deleteFile(item.groupType, item.fileId)}
-        >
+        <div style={{ marginLeft: '5px', marginTop: '10px' }} onClick={() => deleteFile(item.groupType, item.fileId)}>
           <RiDeleteBin5Line color="black" size={15} />
         </div>
       </div>
