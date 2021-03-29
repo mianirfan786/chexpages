d41ab504eb988a10d50262cf53c60919b3bece12
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-03-29 21:18:06 +0500

    changes after merginng

--- src/Components/SurveyModal/index.js
+++ src/Components/SurveyModal/index.js
@@ -6,116 +6,91 @@ import Button from '../Button/index';
 
 import './style.css';
 import '../../App.css';
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    height: ' 100%',
-    backgroundColor: '#12579b',
-    padding: '10px 20px !important',
-    width: '100%',
-  },
-};
 
 const SurveyModal = ({ rating, handleSurveyModal, isSurveyModalVisible, changeRating, handleCheckBox, handleComment, handleSubmitSurvey, surveyModalLoading }) => {
   return (
     <div className="survey-modal">
-     
-        <Modal
-          visible={isSurveyModalVisible}
-          closable={true}
-          title={false}
-          footer={null}
-          onCancel={() => handleSurveyModal(false)}
-          maskClosable={true}
-          maskStyle={{ backgroundColor: 'white' }}
+      <Modal
+        visible={isSurveyModalVisible}
+        closable={true}
+        title={false}
+        footer={null}
+        onCancel={() => handleSurveyModal(false)}
+        maskClosable={true}
+        maskStyle={{ backgroundColor: 'white' }}
         // closeIcon={}
         // bodyStyle={{
         //   width: '100%',
         //   textAlign: 'center',
         //   background: 'linear-gradient(0deg,rgba(27,104,179,0.6) 0%,#1b68b3 99.97%)',
         // }}
-        >
-
-          <div className="server-form-container">
-            <div className="server-form-heading">
-              Save <span className="server-form-price">$5</span> at check out
+      >
+        <div className="server-form-container">
+          <div className="server-form-heading">
+            Save <span className="server-form-price">$5</span> at check out
           </div>
-            <div className="server-form-paragraph">We value your feedback! Help us improve our product by filling our below questionaire.</div>
-            <div className="server-form-sub-heading">
-              How would you rate Chex<span className="server-form-title">.AI</span> app
+          <div className="server-form-paragraph">We value your feedback! Help us improve our product by filling our below questionaire.</div>
+          <div className="server-form-sub-heading">
+            How would you rate Chex<span className="server-form-title">.AI</span> app
           </div>
-            <div className="server-form-rating-stars">
-              <StarRatings
-                rating={rating}
-                starRatedColor="#FF7A00"
-                changeRating={changeRating}
-                starWidthAndHeight="20px"
-                numberOfStars={5}
-                name="rating"
-                starHoverColor="#cecece"
-              />
-            </div>
-            <div className="server-form-question">Which platform would you prefer for vehicle inspections?</div>
+          <div className="server-form-rating-stars">
+            <StarRatings rating={rating} starRatedColor="#FF7A00" changeRating={changeRating} starWidthAndHeight="20px" numberOfStars={5} name="rating" starHoverColor="#cecece" />
+          </div>
+          <div className="server-form-question">Which platform would you prefer for vehicle inspections?</div>
 
-            <div className="server-form-checkboxes">
+          <div className="server-form-checkboxes">
+            <div>
               <div>
-                <div>
-                  <div style={{ display: 'flex' }}>
-                    <label class="newcontainer">
-                      <input onChange={(e) => handleCheckBox('online_services', e)} type="checkbox" />
-                      <span class="newcheckmark"></span>
-                    </label>
-                    <div className="server-form-checkbox-text">
-                      Online Service (Chex<span className="server-form-title">.AI</span>)
-                  </div>
-                  </div>
-                </div>
-
-                <div>
-                  <div style={{ display: 'flex' }}>
-                    <label class="newcontainer">
-                      <input onChange={(e) => handleCheckBox('face_time', e)} type="checkbox" />
-                      <span class="newcheckmark"></span>
-                    </label>
-                    <div className="server-form-checkbox-text">Facetime/Video Chat</div>
-                  </div>
-                </div>
-
-                <div>
-                  <div style={{ display: 'flex' }}>
-                    <label class="newcontainer">
-                      <input onChange={(e) => handleCheckBox('visit_shop', e)} type="checkbox" />
-                      <span class="newcheckmark"></span>
-                    </label>
-                    <div className="server-form-checkbox-text">Visit A Mechanic Shop</div>
+                <div style={{ display: 'flex' }}>
+                  <label class="newcontainer">
+                    <input onChange={(e) => handleCheckBox('online_services', e)} type="checkbox" />
+                    <span class="newcheckmark"></span>
+                  </label>
+                  <div className="server-form-checkbox-text">
+                    Online Service (Chex<span className="server-form-title">.AI</span>)
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div style={{ textAlign: 'center', marginTop: '-7px' }}>
-              <textarea
-                onChange={handleComment}
-                className="server-form-textarea"
-                style={{ resize: 'none' }}
-                id="w3review"
-                name="w3review"
-                placeholder="Any Comments"
-                rows="5"
-                cols="46"
-              ></textarea>
-            </div>
-            <div style={{ marginTop: '8px' }}>
-              <Button onClickButton={handleSubmitSurvey} loading={surveyModalLoading} title={'Next'} buttonClass="survey-button" />
+              <div>
+                <div style={{ display: 'flex' }}>
+                  <label class="newcontainer">
+                    <input onChange={(e) => handleCheckBox('face_time', e)} type="checkbox" />
+                    <span class="newcheckmark"></span>
+                  </label>
+                  <div className="server-form-checkbox-text">Facetime/Video Chat</div>
+                </div>
+              </div>
+
+              <div>
+                <div style={{ display: 'flex' }}>
+                  <label class="newcontainer">
+                    <input onChange={(e) => handleCheckBox('visit_shop', e)} type="checkbox" />
+                    <span class="newcheckmark"></span>
+                  </label>
+                  <div className="server-form-checkbox-text">Visit A Mechanic Shop</div>
+                </div>
+              </div>
             </div>
           </div>
-        </Modal>
-   
+
+          <div style={{ textAlign: 'center', marginTop: '-7px' }}>
+            <textarea
+              onChange={handleComment}
+              className="server-form-textarea"
+              style={{ resize: 'none' }}
+              id="w3review"
+              name="w3review"
+              placeholder="Any Comments"
+              rows="5"
+              cols="46"
+            ></textarea>
+          </div>
+          <div style={{ marginTop: '8px' }}>
+            <Button onClickButton={handleSubmitSurvey} loading={surveyModalLoading} title={'Next'} buttonClass="survey-button" />
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
