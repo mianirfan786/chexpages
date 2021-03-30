9cb8216f90807d632ac34b5104c05c013cc7727a
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-03-30 20:08:54 +0500

    removes unwanted code

--- src/Screens/ContactUsScreen/index.js
+++ src/Screens/ContactUsScreen/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { CardExpiryElement, CardNumberElement, CardCvcElement } from '@stripe/react-stripe-js';
 import { IoIosArrowBack } from 'react-icons/io';
 import { Button, Input } from '../../Components';
-import { Form } from 'antd';
 import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
 
 import './style.css';
 // import '../../App.css';
 
-const ContactUsScreen = ({ priceLoading, handleSubmit, loading, price, buttonDisable }) => {
+const ContactUsScreen = ({ handleSubmit, loading, buttonDisable }) => {
   return (
     <div className="contact-us-main_container">
       <div className="contact-us-container">
@@ -27,26 +25,14 @@ const ContactUsScreen = ({ priceLoading, handleSubmit, loading, price, buttonDis
           </div>
           <div className="text-area-field">
             <form>
-              <textarea
-                className="text-area-styling"
-                rows="4"
-                cols="50"
-                placeholder="Your queries "
-              />
+              <textarea className="text-area-styling" rows="4" cols="50" placeholder="Your queries " />
             </form>
           </div>
-          <Button
-            loading={loading}
-            title={'Submit'}
-            disabled={buttonDisable}
-            onClickButton={handleSubmit}
-            buttonClass="contact-submit-btn"
-          />
+          <Button loading={loading} title={'Submit'} disabled={buttonDisable} onClickButton={handleSubmit} buttonClass="contact-submit-btn" />
           <div></div>
         </div>
         <p className="directlt-reach-text">
-          <span style={{ color: 'white', fontWeight: '600', marginRight: '10px' }}>or</span>You can
-          directly reach us at
+          <span style={{ color: 'white', fontWeight: '600', marginRight: '10px' }}>or</span>You can directly reach us at
         </p>
         <div className="email-container">
           <AiOutlineMail size={24} color="#FF7A00" style={{ marginTop: '5px' }} />
