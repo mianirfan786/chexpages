01db555b822181b1920b259762c96c05469c9f31
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-08-10 17:17:53 +0500

    changehs

--- src/Screens/MaintanceScreen/index.js
+++ src/Screens/MaintanceScreen/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const MaintancePage = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'centers' }}>
-    <h1>Application is Under Maintenance</h1>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'centers', height: '100vh' }}>
+    <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>Application is Under Maintenance</h1>
   </div>
 );
 
