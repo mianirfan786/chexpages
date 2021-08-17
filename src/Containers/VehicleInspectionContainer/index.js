371d8802f79301157ed81e1b4f3fb350c9c679d9
Rahil Ahmad <rahilahmad1258@gmail.com>
2021-08-17 12:11:04 +0500

    fix dateImage(day format)

--- src/Containers/VehicleInspectionContainer/index.js
+++ src/Containers/VehicleInspectionContainer/index.js
@@ -46,7 +46,7 @@ const VehicleInspectionContainer = (props) => {
 
 
   let today = new Date();
-  const date = `${(((today.getMonth() + 1) < 10) ? (`0${today.getMonth() + 1}`) : (today.getMonth() + 1))}-${today.getDate()}-${today.getFullYear()}`;
+  const date = `${(((today.getMonth() + 1) < 10) ? (`0${today.getMonth() + 1}`) : (today.getMonth() + 1))}-${((today.getDate() < 10) ? (`0${today.getDate()}`) : (today.getDate()))}-${today.getFullYear()}`;
   const dateImage = date.toString();
 
   useEffect(() => {
